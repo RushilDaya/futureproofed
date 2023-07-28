13d427bcccf458de2689c744875edb358fc9efa4
rushil.daya <rushil.daya@porphyrio.eu>
2023-07-28 14:31:22 +0200

    added put  endpoint

--- application/crud_api.py
+++ application/crud_api.py
@@ -1,6 +1,8 @@
 # Description: This file contains the CRUD API for the measurement table
 #              It is used to get, update, post and delete measurements
 
+# missing safety features for loading invalid data (units/types etc) should add
+
 from flask import Flask, request
 import sqlite3
 from application import DB_NAME
@@ -38,8 +40,34 @@ def getMeasurement():
 
 @app.route("/measurement/", methods=["PUT"])
 def updateMeasurement():
-    return "Hello, World!"
+    body = request.json
 
+    # check for existing measurement
+    db_conn = sqlite3.connect(DB_NAME)
+    db_cursor = db_conn.cursor()
+    existing_measurement = get_measurement_from_db(
+        body["seic_code"], body["nrg_bal_code"], body["country_code"], body["year"], db_cursor
+    )
+    if existing_measurement is None:
+        response = {"status": "404"}
+        return response
+
+    incoming_measurement = Measurement(
+        seic_code=body["seic_code"],
+        nrg_bal_code=body["nrg_bal_code"],
+        country_code=body["country_code"],
+        year=body["year"],
+        measurement_value=float(body["measurement_value"]),
+        measurement_unit=body["measurement_unit"],
+        standardised_measurement_value=body.get("standardised_measurement_value"),
+        standardised_measurement_unit=body.get("standardised_measurement_unit"),
+    )
+
+    # load the record into the database / standard load method does a replace
+    # if the record exists so this is already in line with the PUT
+    load_record(incoming_measurement, db_cursor, db_conn)
+    response = {"status": "200"}
+    return response
 
 @app.route("/measurement/", methods=["POST"])
 def postMeasurement():
