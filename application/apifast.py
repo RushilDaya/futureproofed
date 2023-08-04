380bf52e6c96b309c3c3fe931b29d82c30467b2b
rushil.daya <rushil.daya@porphyrio.eu>
2023-08-04 09:43:59 +0200

    implemented put

--- application/apifast.py
+++ application/apifast.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
 import sqlite3
 from enum import Enum
 import json
-from typing import List
-from application.data_models.object_models import get_measurement_from_db
+from typing import List, Optional
+from application.data_models.object_models import get_measurement_from_db, Measurement, load_record
 from application import (
     DB_NAME,
     ENERGY_BALANCE_DATA_JSON,
@@ -11,6 +12,8 @@ from application import (
     SEIC_DATA_JSON,
     YEAR_DATA_JSON,
 )
+
+#TODO: the type of YEAR is not being done correctly
 
 app = FastAPI()
 
@@ -21,26 +24,65 @@ def load_keys(file_path: str) -> List:
     keys = list(obj.keys())
     return {str(i): k for i, k in enumerate(keys)}
 
-SEIC_CODES = Enum("SEIC_CODE", load_keys(SEIC_DATA_JSON))
-NRG_BAL_CODES = Enum("NRG_BAL_CODE", load_keys(ENERGY_BALANCE_DATA_JSON))
-COUNTRY_CODES = Enum("COUNTRY_CODE", load_keys(COUNTRY_DATA_JSON))
-YEARS = Enum("YEAR", load_keys(YEAR_DATA_JSON))
 
+SEIC_CODES_ENUM = Enum("SEIC_CODE", load_keys(SEIC_DATA_JSON))
+NRG_BAL_CODES_ENUM = Enum("NRG_BAL_CODE", load_keys(ENERGY_BALANCE_DATA_JSON))
+COUNTRY_CODES_ENUM = Enum("COUNTRY_CODE", load_keys(COUNTRY_DATA_JSON))
+YEARS_ENUM = Enum("YEAR", load_keys(YEAR_DATA_JSON))
+
+class MeasurementBody(BaseModel):
+    # TODO: should be combined with Measurement object
+    seic_code: SEIC_CODES_ENUM
+    nrg_bal_code: NRG_BAL_CODES_ENUM
+    country_code: COUNTRY_CODES_ENUM
+    year: YEARS_ENUM
+    measurement_value: float
+    measurement_unit: str
+    standardised_measurement_value: Optional[float] = None
+    standardised_measurement_unit: Optional[str] = None
 
 @app.get("/measurement", status_code=status.HTTP_200_OK)
 def getMeasurement(
-    seic_code: SEIC_CODES,
-    nrg_bal_code: NRG_BAL_CODES,
-    country_code: COUNTRY_CODES,
-    year: YEARS,
+    seic_code: SEIC_CODES_ENUM,
+    nrg_bal_code: NRG_BAL_CODES_ENUM,
+    country_code: COUNTRY_CODES_ENUM,
+    year: YEARS_ENUM,
     response: Response,
 ):
     db_conn = sqlite3.connect(DB_NAME)
     db_cursor = db_conn.cursor()
 
     measurement = get_measurement_from_db(
-        seic_code.value, nrg_bal_code.value, country_code.value, year.value, db_cursor
+        seic_code.value, nrg_bal_code.value, country_code.value, int(year.value), db_cursor
     )
     if measurement == None:
         response.status_code = status.HTTP_404_NOT_FOUND
     return measurement.pretty()
+
+@app.put("/measurement", status_code=status.HTTP_200_OK)
+def updateMeasurement(body: MeasurementBody, response: Response):
+    db_conn = sqlite3.connect(DB_NAME)
+    db_cursor = db_conn.cursor()
+    existing_measurement = get_measurement_from_db(
+        body.seic_code.value, 
+        body.nrg_bal_code.value, 
+        body.country_code.value, 
+        int(body.year.value), 
+        db_cursor
+    )
+    if existing_measurement is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return
+    
+    incoming_measurement = Measurement(
+        seic_code=body.seic_code.value,
+        nrg_bal_code=body.nrg_bal_code.value,
+        country_code=body.country_code.value,
+        year=int(body.year.value),
+        measurement_value=body.measurement_value,
+        measurement_unit=body.measurement_unit,
+        standardised_measurement_value=body.standardised_measurement_value,
+        standardised_measurement_unit=body.standardised_measurement_unit
+    )
+    load_record(incoming_measurement, db_cursor, db_conn)
+    return
